184d794425bf90df9a4dae4cc175fc2dda6cb8b2
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-02-01 13:33:49 -0700

    added query playground, GET todos route, GET todos tests

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,28 @@
+const {ObjectId} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = '589241cf108c341af02e5d280';
+
+if (!ObjectId.isValid(id)){
+	console.log('ID not valid');
+}
+
+/* Todo.find({
+	_id: id
+}).then((todos)=> {
+	console.log('Todos',todos);
+});
+
+Todo.findOne({
+	_id: id
+}).then((todo)=> {
+	console.log('Todo',todo);
+}); */
+
+Todo.findById(id).then((todo)=> {
+	if (!todo){
+		return console.log('ID not found');
+	}
+	console.log('Todo by ID',todo);
+}).catch((e) => console.log(e) );
