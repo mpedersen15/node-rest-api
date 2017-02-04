7078aa781cf3941e94d98d415bac5e03ebeb28dc
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-02-04 12:17:18 -0700

    Added tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
 	text: 'First test todo'
 },{
 	_id: new ObjectID(),
-	text: 'Second test todo'
+	text: 'Second test todo',
+	completed: true,
+	completedAt: new Date().getTime()
 }]
 
 beforeEach( (done) => {
@@ -140,4 +142,38 @@ describe('DELETE /todos/:id', ()=>{
 			.expect(404)
 			.end(done);
 	});
-})
+});
+
+describe('PATCH /todos/:id', () =>{
+	it('should  update the todo', (done) => {
+		var text = "Update task completed to true";
+		request(app)
+			.patch('/todos/' + todos[0]._id.toHexString())
+			.send({
+				text,
+				completed: true
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.completed).toBe(true);
+				expect(res.body.todo.completedAt).toBeA('number');
+			}).end(done);
+	});
+	
+	it('should clear completedAt when todo is not completed', (done) => {
+		var text = "Update task completed to false";
+		request(app)
+			.patch('/todos/' + todos[1]._id.toHexString())
+			.send({
+				text,
+				completed: false
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.completed).toBe(false);
+				expect(res.body.todo.completedAt).toNotExist();
+			}).end(done);
+	});
+});
