fb8fccbd8c13a76b99e59067167ef8c0bd7d88a1
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-03-05 12:33:10 -0700

    Added x-auth to headers allows in CORS

--- server/server.js
+++ server/server.js
@@ -26,8 +26,10 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization, x-auth');
 
+	res.setHeader('Access-Control-Expose-Headers', 'x-auth');
+	
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
